0e023f8ad3a80cc5816b66413c6fbbb55299e6e2
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-09-26 10:48:40 -0500

    basic html layout

---
+++ assets/javascript/java.js
@@ -0,0 +1,34 @@
+var config =
+{
+	apiKey: "AIzaSyCSAWsanixG4wQmVMGfgscaqfcl57et_XY",
+	authDomain: "train-67d4a.firebaseapp.com",
+	databaseURL: "https://train-67d4a.firebaseio.com",
+	projectId: "train-67d4a",
+	storageBucket: "",
+	messagingSenderId: "315319093054"
+};
+  
+firebase.initializeApp(config);
+
+
+
+$(document).ready(function()
+{
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})	
