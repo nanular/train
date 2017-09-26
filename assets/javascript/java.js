f8e935068152f560fdb346b3b9c715bea3b560bf
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-09-26 14:53:47 -0500

    firebase functionality

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,3 +1,6 @@
+$(document).ready(function()
+{
+	
 var config =
 {
 	apiKey: "AIzaSyCSAWsanixG4wQmVMGfgscaqfcl57et_XY",
@@ -9,19 +12,47 @@ var config =
 };
   
 firebase.initializeApp(config);
+var database = firebase.database();
 
-
-
-$(document).ready(function()
+var currentTrain = 
 {
-	
+	name: "",
+	destination: "",
+	time: "",
+	frequency: 0
+};
 
 
 
+$("#train_submit").click(function()
+{
+	event.preventDefault();
+
+	currentTrain.name = $("#train_name").val().trim();
+	currentTrain.destination = $("#train_destination").val().trim();
+	currentTrain.time = $("#train_time").val().trim();
+	currentTrain.frequency = $("#train_frequency").val().trim();
+
+	database.ref().push(currentTrain);
+
+	$("form").trigger("reset");
 
 
+})
+
+$("#clear_button").click(function()
+{
+	$("form").trigger("reset");
+})
 
 
+database.ref().on("child_added", function(snapshot)
+{
+	console.log(snapshot.val());
+
+	$("#train_table").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().destination +
+		"</td><td>" + snapshot.val().frequency + "</td></tr>")
+})
 
 
 
