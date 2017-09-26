7d01b508e22f52dcf9dd3fc191f7177e541be82e
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-09-26 17:01:37 -0500

    added moment.js

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -50,8 +50,47 @@ database.ref().on("child_added", function(snapshot)
 {
 	console.log(snapshot.val());
 
+	var currentHours = moment().hours();
+  	var currentMinutes = moment().minutes();
+  	var totalMinutes = currentHours * 60 + currentMinutes;
+  	console.log(totalMinutes);
+
+	var firstArrival = snapshot.val().time;
+	firstArrival = moment(firstArrival, "HH:mm");
+	var firstTrainHours = firstArrival.hours();
+	var firstTrainMin = firstArrival.minutes();
+	var firstTrainTotalMin = (firstTrainHours * 60) + firstTrainMin;
+	console.log(firstTrainTotalMin);
+
+	var nextArrival = "";
+	var minutesUntilArrival = "";
+
+	if(totalMinutes < firstTrainTotalMin)
+	{
+		nextArrival = firstTrainTotalMin;
+	} else
+	{
+		var completedTrips = Math.floor((totalMinutes - firstTrainTotalMin)/snapshot.val().frequency);
+		nextArrival = ((completedTrips + 1) * snapshot.val().frequency) + firstTrainTotalMin;
+	}
+
+	console.log(nextArrival);
+	minutesUntilArrival = nextArrival - totalMinutes;
+	var nextArrivalHours = Math.floor(nextArrival / 60);
+	var nextArrivalMin = nextArrival % 60;
+	if (nextArrivalMin < 10)
+	{
+		var nextArrivalMinEdit = "0" + nextArrivalMin;
+	} else
+	{
+		nextArrivalMinEdit = nextArrivalMin;
+	}
+	var nextArrivalTime = nextArrivalHours + ":" + nextArrivalMinEdit;
+
+
 	$("#train_table").append("<tr><td>" + snapshot.val().name + "</td><td>" + snapshot.val().destination +
-		"</td><td>" + snapshot.val().frequency + "</td></tr>")
+		"</td><td>" + snapshot.val().frequency + "</td><td>" + nextArrivalTime + "</td><td>" +
+		minutesUntilArrival + "</td></tr>")
 })
 
 
