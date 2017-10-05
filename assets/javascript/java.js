9998ffa87a92943ceb6ed3b3963b03245ae6914a
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-10-05 13:52:30 -0500

    added input validation

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -24,19 +24,119 @@ var currentTrain =
 
 
 
+
+
+	$("#train_name").on("input", function()
+	{
+
+		var input = $(this);
+		var isNameValid = input.val();
+
+		if (isNameValid)
+		{
+			input.removeClass("invalid").addClass("valid");
+		} else
+		{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+
+	$("#train_destination").on("input", function()
+	{
+		var input = $(this);
+		var isDestinationValid = input.val();
+		if (isDestinationValid)
+		{
+			input.removeClass("invalid").addClass("valid");
+		} else
+		{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+
+	$("#train_time").on("input", function()
+	{
+		var input = $(this);
+		var military_regex = /^([01]\d|2[0-3]):?([0-5]\d)$/;
+		var isTimeValid = military_regex.test(input.val());
+
+		if (isTimeValid)
+		{
+			input.removeClass("invalid").addClass("valid");
+		} else
+		{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+
+	$("#train_frequency").on("input", function()
+	{
+		var input = $(this);
+		var integer_regex = /^\d*$/;
+		var isFrequencyValid = integer_regex.test(input.val());
+		var input_data = input.val();
+
+		if (isFrequencyValid && input_data != 0)
+		{
+			input.removeClass("invalid").addClass("valid");
+		} else
+		{
+			input.removeClass("valid").addClass("invalid");
+		}
+	});
+
+
+
+
 $("#train_submit").click(function()
 {
 	event.preventDefault();
 
-	currentTrain.name = $("#train_name").val().trim();
-	currentTrain.destination = $("#train_destination").val().trim();
-	currentTrain.time = $("#train_time").val().trim();
-	currentTrain.frequency = $("#train_frequency").val().trim();
+	if ($("#train_name").hasClass("valid"))
+	{
+		currentTrain.name = $("#train_name").val().trim();
+		isNameValid = true;
+	} else
+	{
+		isNameValid = false;
+		alert("Please enter a name for your train.");
+	}
 
-	database.ref().push(currentTrain);
+	if ($("#train_destination").hasClass("valid"))
+	{
+		currentTrain.destination = $("#train_destination").val().trim();
+		isDestinationValid = true;
+	} else
+	{
+		isDestinationValid = false;
+		alert("Please enter a destination for your train.");
+	}
 
-	$("form").trigger("reset");
+	if ($("#train_time").hasClass("valid"))
+	{
+		currentTrain.time = $("#train_time").val().trim();
+		isTimeValid = true;
+	} else
+	{
+		isTimeValid = false;
+		alert("Please enter a valid arrival time for your train using military time.");
+	}
 
+	if ($("#train_frequency").hasClass("valid"))
+	{
+		currentTrain.frequency = $("#train_frequency").val().trim();
+		isFrequencyValid = true;
+	} else
+	{
+		isFrequencyValid = false;
+		alert("Please enter an interger greater than zero to set the frequency of your train.");
+	}	
+
+	if (isNameValid && isDestinationValid && isTimeValid && isFrequencyValid)
+	{
+		database.ref().push(currentTrain);
+		$("form").trigger("reset");
+	}
 
 })
 
@@ -48,19 +148,19 @@ $("#clear_button").click(function()
 
 database.ref().on("child_added", function(snapshot)
 {
-	console.log(snapshot.val());
+	//console.log(snapshot.val());
 
 	var currentHours = moment().hours();
   	var currentMinutes = moment().minutes();
   	var totalMinutes = currentHours * 60 + currentMinutes;
-  	console.log(totalMinutes);
+  	//console.log(totalMinutes);
 
 	var firstArrival = snapshot.val().time;
 	firstArrival = moment(firstArrival, "HH:mm");
 	var firstTrainHours = firstArrival.hours();
 	var firstTrainMin = firstArrival.minutes();
 	var firstTrainTotalMin = (firstTrainHours * 60) + firstTrainMin;
-	console.log(firstTrainTotalMin);
+	//console.log(firstTrainTotalMin);
 
 	var nextArrival = "";
 	var minutesUntilArrival = "";
@@ -74,7 +174,7 @@ database.ref().on("child_added", function(snapshot)
 		nextArrival = ((completedTrips + 1) * snapshot.val().frequency) + firstTrainTotalMin;
 	}
 
-	console.log(nextArrival);
+	//console.log(nextArrival);
 	minutesUntilArrival = nextArrival - totalMinutes;
 	var nextArrivalHours = Math.floor(nextArrival / 60);
 	var nextArrivalMin = nextArrival % 60;
